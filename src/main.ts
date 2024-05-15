27602fdc8f26aba83bcf09be035d8222f227add6
Sergey Golovan <golovansm@nornik.ru>
2024-05-16 00:08:06 +0300

    fix[main] refactor shutdown handlers

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.on('SIGINT', async () => {
+  const exitWithCode = (code: number) => async () => {
     await app.close();
     setTimeout(() => {
-      process.exit(0);
+      process.exit(code);
     });
-  });
-  process.on('SIGTERM', async () => {
-    await app.close();
-    setTimeout(() => {
-      process.exit(0);
-    });
-  });
+  };
 
-  process.on('uncaughtException', async (e) => {
-    console.error(e);
-    await app.close();
-    setTimeout(() => {
-      process.exit(1);
-    });
-  });
-
-  process.on('unhandledRejection', async (e) => {
-    console.error(e);
-    await app.close();
-    setTimeout(() => {
-      process.exit(1);
-    });
-  });
+  process.on('SIGINT', exitWithCode(0));
+  process.on('SIGTERM', exitWithCode(0));
+  process.on('uncaughtException', exitWithCode(1));
+  process.on('unhandledRejection', exitWithCode(1));
 
   await app.listen(3002);
 }
